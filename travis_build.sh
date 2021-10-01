db54ac4fe5a80165b119b84864abf26a3fb667a1
hqnghi88 <hqnghi88@gmail.com>
2021-10-01 13:09:35 -0700

    ci deploy

--- travis_build.sh
+++ travis_build.sh
@@ -76,7 +76,7 @@ generate_p2updatesite_category
 #git_push
 
 cd msi.gama.experimental.parent &&
-mvn clean install -X -U -P p2Repo --settings ../settings.xml&&
+mvn clean install -U -P p2Repo --settings ../settings.xml&&
 cd -
 
 #cd msi.gama.experimental.p2updatesite &&
