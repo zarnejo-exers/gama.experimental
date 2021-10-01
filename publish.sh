0fa6e2b8ec849b04ef892602c70ea212ad39279a
hqnghi88 <hqnghi88@gmail.com>
2021-10-01 09:11:46 -0700

    ci deploy

--- publish.sh
+++ publish.sh
@@ -2,27 +2,6 @@
 
 deploy(){	
 
-cat ~/.m2/settings.xml 
-
-
-header=$(<$GITHUB_WORKSPACE/settings_header.xml)
-core=$(<$GITHUB_WORKSPACE/settings_auth.xml)
-footer=$(<$GITHUB_WORKSPACE/settings_footer.xml) 
-echo "$header $core $footer"> $GITHUB_WORKSPACE/settings.xml
-
-
-echo "DEPLOY" 
-mkdir -m 0700 -p ~/.ssh 
-chmod 750 ~
-chmod 700 ~/.ssh 
-echo -e "Host *\n" >> ~/.ssh/config 
-echo -e "IdentitiesOnly yes\n" >> ~/.ssh/config 
-echo -e "Port 22\n" >> ~/.ssh/config 
-echo -e "StrictHostKeyChecking no\n" >> ~/.ssh/config 
-echo -e "PubkeyAuthentication no\n" >> ~/.ssh/config 
-cat ~/.ssh/config 
-ssh-keyscan -H $SSH_HOST >> ~/.ssh/known_hosts
-
 cd msi.gama.experimental.parent && 
 mvn install -P p2Repo --settings ../settings.xml && 
 cd -
